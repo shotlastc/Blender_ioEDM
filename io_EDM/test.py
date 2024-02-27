f6178414ef18cba634efa3724af4fb4734967ea3
733TH <touchpoker6@gmail.com>
2024-02-27 17:22:08 +0200

    for edm reading test

---
+++ io_EDM/test.py
@@ -0,0 +1,8 @@
+from utils import chdir, print_edm_graph
+
+from edm.types import EDMFile
+
+if __name__ == '__main__':
+  #edm = EDMFile("D:\\dcs\\Ka-50_3.edm")
+  edm = EDMFile("D:\\dcs\\f-14b.edm")
+  #print_edm_graph(edm.nodes[0])
